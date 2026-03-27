3443c1f1e0d5845c83f50486efb04d0684624191
XolldaVeres <XolldaVeres@users.noreply.github.com>
2026-03-27 11:19:08 +1000

    Fix: add requirements.txt and fix flask import

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,15 @@
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Backend!"
+    return "Hello from Backend! Docker + Nginx works!"
 
 @app.route('/api')
 def api():
-    return {"status": "ok", "message": "Docker + Nginx works!"}
+    return jsonify({"status": "ok", "message": "Docker + Nginx works!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
